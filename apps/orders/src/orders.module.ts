d5c476caa81f67fe607ec766fc45fbc42ee03a8e
Austine Onyema <austine574@gmail.com>
2026-03-31 12:42:21 +0100

    added orders schema, order repository and set up database module in module.ts

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
-
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
+import { DatabaseModule } from '@app/common';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_DATABASE_URL: Joi.string().required(),
+      }),
+      envFilePath: '../.env',
+    }),
+    DatabaseModule,
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
